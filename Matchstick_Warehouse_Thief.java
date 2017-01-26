66613db16de4555766d79a3ea67f4a16cc8207bf
Karan Arora <arora15karan1996@gmail.com>
2017-01-27 01:44:42 +0530

    Update Matchstick_Warehouse_Thief.java

--- Matchstick_Warehouse_Thief.java
+++ Matchstick_Warehouse_Thief.java
@@ -1,24 +1,27 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 class crate
 {
     int boxes;
     int sticks;
     
-    fire( int box, int st)
+    crate( int box, int st)
     {
+        System.out.println(" "+box + " "+sticks);
         this.boxes = box;
         this.sticks = st;
     }
-    public int compareTo()
+    public int compareTo( Object o)
     {
-        
+        System.out.println("Hello 1");
+        crate c = (crate)o;
+        if( this.sticks < c.sticks )
+            return -1;
+          else if( this.sticks < c.sticks )
+              return 1;
+            else
+              return 0;
     }
-
 }
 public class Solution 
 {
@@ -27,11 +30,31 @@ public class Solution
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int crates = in.nextInt();
-        PriorityQueue<crate> pq = new PriorityQueue<Crate>(n, Collections.reverseOrder() ); 
-        for(int crate_i=0; crate_i < c; crate_i++)
+        PriorityQueue<crate> pq = new PriorityQueue<crate>(n, Collections.reverseOrder() ); 
+        for(int i = 0; i < crates; i++)
         {
-            crate c = new crate(sc.nextInt(), sc.nextInt() );
+            crate c = new crate( in.nextInt(), in.nextInt() );
+            pq.add( c );
         }
+        
+        int temp = n;
+        int result = 0;
+        while( temp > 0 )
+        {
+            crate c = pq.poll();
+            System.out.println(" box "+ c.boxes+"st "+c.sticks);
+            if(c.boxes < temp )
+            {
+                result += c.boxes * c.sticks;
+                temp -= c.boxes;
+            }
+              else if( c.boxes > temp )
+              {
+                  result += (temp) * c.sticks;
+                  temp = 0;
+              }
+        }
+        System.out.println( result );
         // your code goes here
     }
 }
