f4dc0153f18597e9c5a16219dcf39942fd1df884
Karan Arora <arora15karan1996@gmail.com>
2017-01-27 02:54:03 +0530

    Update Matchstick_Warehouse_Thief.java

--- Matchstick_Warehouse_Thief.java
+++ Matchstick_Warehouse_Thief.java
@@ -1,28 +1,27 @@
 import java.util.*;
 
-class crate
+class crate implements Comparable
 {
     int boxes;
     int sticks;
     
     crate( int box, int st)
     {
-        System.out.println(" "+box + " "+sticks);
         this.boxes = box;
         this.sticks = st;
     }
     public int compareTo( Object o)
     {
-        System.out.println("Hello 1");
         crate c = (crate)o;
         if( this.sticks < c.sticks )
             return -1;
-          else if( this.sticks < c.sticks )
+          else //( this.sticks < c.sticks )
               return 1;
-            else
-              return 0;
+            //else
+             // return 0;
     }
 }
+
 public class Solution 
 {
     public static void main(String[] args) 
@@ -30,31 +29,40 @@ public class Solution
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int crates = in.nextInt();
-        PriorityQueue<crate> pq = new PriorityQueue<crate>(n, Collections.reverseOrder() ); 
+        PriorityQueue<crate> pq = new PriorityQueue<crate>(crates, Collections.reverseOrder() ); 
+        int a, b, total_a = 0, total =0;
         for(int i = 0; i < crates; i++)
         {
-            crate c = new crate( in.nextInt(), in.nextInt() );
+            a = in.nextInt(); 
+            b = in.nextInt();
+            total += a*b;
+            total_a += a;
+            crate c = new crate( a, b );
             pq.add( c );
         }
-        
-        int temp = n;
-        int result = 0;
-        while( temp > 0 )
-        {
-            crate c = pq.poll();
-            System.out.println(" box "+ c.boxes+"st "+c.sticks);
-            if(c.boxes < temp )
+        if(total_a < n )
+            System.out.println(total);
+        else
+       {
+            int temp = n;
+            int result = 0;
+            while( temp > 0 )
             {
-                result += c.boxes * c.sticks;
-                temp -= c.boxes;
+                crate c = pq.poll();
+                if(c.boxes <= temp )
+                {
+                    result += c.boxes * c.sticks;
+                    temp -= c.boxes;
+                }                    
+                else if( c.boxes > temp )
+                {
+                    result += (temp) * c.sticks;
+                    break;
+                }
             }
-              else if( c.boxes > temp )
-              {
-                  result += (temp) * c.sticks;
-                  temp = 0;
-              }
+            System.out.println( result );
+
         }
-        System.out.println( result );
-        // your code goes here
     }
+    
 }
